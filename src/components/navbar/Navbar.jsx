86da1334d82ed7d2eaeb8c5124a26b7c5c307d6a
Menubatenny <tenny1998@gmail.com>
2024-01-19 06:56:24 +0000

    title visible onscroll; hover effect

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,9 +5,21 @@ import {getImageUrl} from "../../utils"
 
 export const Navbar = () => {
     const [ menuOpen, setMenuOpen] = useState(false);
+    const [fix, setFix] = useState(false)
+
+    function setFixed() {
+        if(window.scrollY >= 150) {
+            setFix(true)
+        } else {
+            setFix(false)
+        }
+    }
+
+    window.addEventListener("scroll", setFixed)
+
     return (
     <nav className= {styles.navbar}>
-        <a className= {styles.title} href="/">Menuba tenny</a>
+        <a className= {fix ? `${styles.title} ${styles.fixed}` : styles.title } href="/">Menuba tenny</a>
         <div className= {styles.menu}> 
             <img className={styles.menuBtn} 
                 src={ menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
@@ -25,6 +37,9 @@ export const Navbar = () => {
                 </li>
                 <li>
                     <a href="#project"> Project </a>
+                </li>
+                <li>
+                    <a href="#blog"> Blog </a>
                 </li>
                 <li>
                     <a href="#contact"> Contact </a>

0abb87c7254d8cff9865dfcd990e72ec125785a8
Menubatenny <tenny1998@gmail.com>
2024-01-22 23:22:57 +0000

    add tooltip

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react"
+import React from "react"
 import { getImageUrl } from "../../utils"
 import styles from "./home.module.css"
 import { motion} from "framer-motion"
@@ -18,15 +18,34 @@ export const Home = () => {
         <h1 className={styles.title}>  Menuba tenny </h1>
         <p className={styles.description}> I'm a Full-stack web developer with over 2+ years of experience using Ruby on Rails, elixir, and React. I'm also a graduated GIS Analyst. Reach out to me, If you would like to know more! </p>
         <a className={styles.contactbtn} href="mailto:menubatenny@gmail.com">Contact me</a>
-        <div className={styles.stack}><h2> Tech stack |   </h2>
-        <h1 className={styles.icons}>
-        <img src={getImageUrl("icons/react-.png")}/>
-        <img src={getImageUrl("icons/nodejs.png")}/>
-        <img src={getImageUrl("icons/elixir.png")}/>
-        <img src={getImageUrl("icons/ror.png")}/>
-        <img src={getImageUrl("icons/python.png")}/>
-        <img src={getImageUrl("icons/git.png")}/>
-        </h1>
+        <div className={styles.stack}>
+            <h2> Tech stack |   </h2>
+            <div className={styles.wrapper}>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/react-.png")}/>
+                    <div className={styles.tooltip}> React </div>
+                </div>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/nodejs.png")}/>
+                    <div className={styles.tooltip}> NodeJs </div>
+                </div>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/elixir.png")}/>
+                    <div className={styles.tooltip}> Elixir </div>
+                </div>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/ror.png")}/>
+                    <div className={styles.tooltip}> RubyonRails </div>
+                </div>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/python.png")}/>
+                    <div className={styles.tooltip}> Python </div>
+                </div>
+                <div className={styles.icon}>
+                    <img src={getImageUrl("icons/git.png")}/>
+                    <div className={styles.tooltip}> Git </div>
+                </div>
+            </div>
         </div>
     </motion.div>
 
